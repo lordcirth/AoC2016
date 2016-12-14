ef4c028817ce9467e1c83ce12e3715c9cb601a94
Nathan Fish <lordcirth@gmail.com>
2016-12-13 20:03:04 -0500

    refactor to use custom datatypes

--- day1/day1.hs
+++ day1/day1.hs
@@ -4,15 +4,16 @@
 
 main = do
     rawInput <- readFile "./input"
-    putStrLn $ show $ followPath (0,0) (parseInput rawInput)
+    putStrLn $ show $ followPath starting_pos (parseInput rawInput)
 
 
-type Move = (Char, Int)
+type Move = (Rotation, Int)
 
-data Direction  = North | South | East | West
-data Position   = Position { x,y :: Int, dir :: Direction }
+data Direction  = North | South | East | West deriving Show
+data Position   = Position { x,y :: Int, direction :: Direction } deriving Show
+data Rotation   = L | R deriving (Read, Show)
 
-starting_pos = Position {x = 0, y = 0, dir = North }
+starting_pos = Position {x = 0, y = 0, direction = North }
 
 -- Step 1: Parse the input into a useful form: Done
 
@@ -25,7 +26,7 @@ parseInput rawInput =
 
 parseMove :: String -> Move
 parseMove (dir:num) =
-    (dir, read num :: Int)
+    (read [dir] :: Rotation, read num :: Int)
 
 
 -- Step 2: Find the resulting coordinates of the given path
@@ -37,15 +38,30 @@ followPath pos []           = pos
 
 -- main recursive function
 followPath pos (m:moves)    =
-    followPath move dir pos m
+--    followPath move pos m
+    pos
 
 
 doMove :: Position -> Move -> Position
 doMove pos move = pos -- boilerplate!
 
 
--- rotateDir
+rotateDir :: Direction -> Char -> Direction
+rotateDir pos 'L' =
+    case (pos) of
+        North   -> West
+        West    -> South
+        South   -> East
+        East    -> North
 
+rotateDir pos 'R' =
+    case (pos) of
+        North   -> East
+        East    -> South
+        South   -> West
+        West    -> North
+
+rotateDir _ _ = error "Only rotations L and R are valid!"
 
 -- Step 3: Find the shortest path to those coordinates
 
