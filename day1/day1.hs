cc71060292393a45bbfb6c565722eb63088233d2
Nathan Fish <lordcirth@gmail.com>
2016-12-13 19:18:07 -0500

    add comments and gitignore

--- day1/day1.hs
+++ day1/day1.hs
@@ -1,12 +1,13 @@
 -- Advent of Code day 1
--- Find the shortest path, and print the distance, equivalent to a given sequence of movements
--- Using "taxicab geometry"
-
--- First step: Parse the input into a useful form
+-- Find the shortest path equivalent to a given sequence of movements
+-- and print the length of it, using "taxicab geometry"
 
 main = do
     rawInput <- readFile "./input"
     putStrLn $ show $ parseInput rawInput
+
+
+-- Step 1: Parse the input into a useful form
 
 --parseInput :: String -> [(Char, Int)]
 parseInput rawInput =
@@ -17,3 +18,10 @@ parseInput rawInput =
 parseMove :: String -> (Char, Int)
 parseMove (dir:num) =
     (dir, read num :: Int)
+
+-- Step 2: Find the resulting coordinates of the given path
+
+
+-- Step 3: Find the shortest path to those coordinates
+
+-- Step 4: Print it's length.
